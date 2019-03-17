a0089e27475391b14b375185b8c7d01177fa5041
William <william@pixney.com>
2019-03-17 15:05:29 +0100

    .

--- resources/js/src/excard.js
+++ resources/js/src/excard.js
@@ -1,123 +1,43 @@
 const cards = document.querySelectorAll('.m-excards__card');
 const collapsers = document.querySelectorAll('.js-collapser');
+const cells = document.getElementsByClassName('m-excards__card');
+
+
+function collapse(thisCell) {
+    for (let cell of cells) {
+        cell.classList.remove('is-inactive');
+    }
+    thisCell.classList.remove('is-expanded');
+    thisCell.classList.add('is-collapsed');
+}
 
 Array.from(cards).forEach(card => {
 
     card.addEventListener('click', function (event) {
-
+        
         const thisCell = event.target.closest(".m-excards__card");
 
-        if (thisCell.classList.contains('is-collapsed')) {
+        if (event.target.classList.contains('js-collapser')) {    
 
-            const cells = document.getElementsByClassName('m-excards__card');
+            collapse(thisCell);
+            
+        } else if (thisCell.classList.contains('is-collapsed')) {
+
             for (let cell of cells) {
                 cell.classList.remove('is-expanded');
                 cell.classList.add('is-collapsed');
                 cell.classList.add('is-inactive');
             }
 
+            thisCell.classList.remove('is-inactive');
             thisCell.classList.remove('is-collapsed');
             thisCell.classList.add('is-expanded');
 
 
         } else {
-
-            console.log('Does not.');
-            // const cells = document.getElementsByClassName('m-excards__card');
-            // for (let cell of cells) {
-            //     cell.classList.remove('is-inactive');
-            // }
-
-            // thisCell.classList.remove('is-expanded');
-            // thisCell.classList.add('is-collapsed');
-
+            // Use if we would like to use the button to toggle collapsed.
         }
 
 
     });
 });
-
-Array.from(collapsers).forEach(collapse => {
-
-    collapse.addEventListener('click', function (event) {
-        const thisCell = event.target.closest(".m-excards__card");
-        const cells = document.getElementsByClassName('m-excards__card');
-        
-        for (let cell of cells) {
-            cell.classList.remove('is-inactive');
-        }
-
-        thisCell.classList.remove('is-expanded');
-        thisCell.classList.add('is-collapsed');
-    });
-});
-
-
-
-// var deleteLinks = document.querySelectorAll('.m-excards__card');
-
-// Array.from(deleteLinks).forEach(link => {
-//     link.addEventListener('click', function (event) {
-//         // if (!confirm(`sure u want to delete ${this.title}`)) {
-//         //     event.preventDefault();
-//         // }
-//         console.log('hello');
-//     });
-// });
-
-
-// document.addEventListener('click', function (event) {
-
-
-//     // If the clicked element doesn't have the right selector, bail
-
-//     if (!event.target.matches('.js-expander')) return;
-//     console.log(event.target);
-//     const thisCell = event.target.closest(".m-excards__card");
-
-
-
-
-//     if (thisCell.classList.contains('is-collapsed')) {
-
-//         const cells = document.getElementsByClassName('m-excards__card');
-//         for (let cell of cells) {
-//             cell.classList.remove('is-expanded');
-//             cell.classList.add('is-collapsed');
-//             cell.classList.add('is-inactive');
-//         }
-
-//         thisCell.classList.remove('is-collapsed');
-//         thisCell.classList.add('is-expanded');
-
-
-//     } else {
-
-//         console.log('Does not.');
-//         const cells = document.getElementsByClassName('m-excards__card');
-//         for (let cell of cells) {
-//             cell.classList.remove('is-inactive');
-//         }
-
-//         thisCell.classList.remove('is-expanded');
-//         thisCell.classList.add('is-collapsed');
-
-//     }
-
-// }, false);
-
-// document.addEventListener('click', function (event) {
-//     // If the clicked element doesn't have the right selector, bail
-//     if (!event.target.matches('.js-collapser')) return;
-
-//     const thisCell = event.target.closest(".m-excards__card");
-
-//     const cells = document.getElementsByClassName('m-excards__card');
-//     for (let cell of cells) {
-//         cell.classList.remove('is-inactive');
-//     }
-
-//     thisCell.classList.remove('is-expanded');
-//     thisCell.classList.add('is-collapsed');
-
-// }, false);
